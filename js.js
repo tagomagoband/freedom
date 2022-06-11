6780b86dfde040ea8527968ffcca96d2925f0a69
sserolf <34779246+sserolf@users.noreply.github.com>
2022-06-11 02:02:29 +0200

    First commit

---
+++ js.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  const spoti = document.getElementById('spoti');
+  const yt = document.getElementById('yt');
+  const apple = document.getElementById('apple');
+  spoti.onclick = () => {
+    window.open('https://open.spotify.com/album/5m3M7lp4Uce8IUt3C2rqay?si=qOREKFKgQeaQW3uEn0pu2A');
+  }
+  yt.onclick = () => {
+    window.open('https://music.youtube.com/watch?v=r4CrGzfmWck&feature=share');
+  }
+  apple.onclick = () => {
+    window.open('https://music.apple.com/es/album/freedom/1626924381?i=1626924383');
+  }
+}
