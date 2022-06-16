f1baf32f2bd5cf9f82c559a1f6a874d630eac67a
tagomagoband <90961570+tagomagoband@users.noreply.github.com>
2022-06-16 14:45:10 +0200

    Added youtube video logo (html, css, js)

--- js.js
+++ js.js
@@ -2,6 +2,8 @@ window.onload = () => {
   const spoti = document.getElementById('spoti');
   const yt = document.getElementById('yt');
   const apple = document.getElementById('apple');
+  const yt_video = document.getElementById('yt_video');
+
   spoti.onclick = () => {
     window.open('https://open.spotify.com/album/5m3M7lp4Uce8IUt3C2rqay?si=qOREKFKgQeaQW3uEn0pu2A');
   }
@@ -10,5 +12,8 @@ window.onload = () => {
   }
   apple.onclick = () => {
     window.open('https://music.apple.com/es/album/freedom/1626924381?i=1626924383');
+  }
+  yt_video.onclick = () => {
+    window.open('https://youtu.be/Xx1YGOpxi1w');
   }
 }
